84c98516b9683465b2b0c1f105347838bbd8bc03
Chinmay Roy <39342322+chinmayroy@users.noreply.github.com>
2022-11-11 22:43:42 +0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tools',
-    'version': '0.1',
+    'version': '15.0.1',
     'sequence': 1,
 
     # any module necessary for this one to work correctly
