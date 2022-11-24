3b4c39aa9cdae935cdcb57568c7cdbbf0feb8284
Chinmay Roy <mrchinmayroy@gmail.com>
2022-11-24 18:46:12 +0600

    update

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -19,31 +19,29 @@ class VenueBooking(http.Controller):
     @http.route('/booking/bill_pay', auth='user', website=True)
     def bill_pay(self):
         partner_id = request.env['res.users'].browse()
-        currency_id = request.env['account.move'].browse()
-        if not currency_id:
-            currency_id = request.env.ref['base.USD'].id
         p_name = request.env['product.product'].browse()
-        product_id = request.env['product.product'].browse()
+        product_id = request.env['venue.list'].sudo().search([])
         price_unit = request.env['product.product'].browse()
-        tax_ids = request.env['product.product'].browse()
         today = datetime.datetime.now()
-        return request.env['account.move'].create({
+        invoice = request.env['account.move'].create({
             'move_type': 'in_invoice',
             'date': '2017-01-01',
             'partner_id': partner_id,
             'invoice_date': today,
-            'currency_id': currency_id,
             'invoice_line_ids': [
                 (0, None, {
                     'name': p_name,
                     'product_id': product_id,
                     'quantity': 1,
                     'price_unit': price_unit,
-                    'tax_ids': tax_ids,
                 }),
             ]
         })
-        # return request.render("venue_booking.booking_bill_pay", move)
+        print("=================================================")
+        print(invoice)
+        print("=================================================")
+        # return invoice
+        # return request.render("venue_booking.booking_bill_pay", invoice)
 
     @http.route('/booking/thank_you_message', auth='user', website=True)
     def thankyou(self, **kw):
--- models/venue_lists.py
+++ models/venue_lists.py
@@ -7,15 +7,8 @@ class VenueList(models.Model):
     _description = 'Venue List'
 
     name = fields.Char(string="Venue Name")
-    product_id = fields.Char(string='Product ID', required=True, readonly=True, default=lambda self: _('New'), copy=False)
+    product_id = fields.Many2one('product.product', string='Product ID')
     team_name = fields.Many2one("venue.booking.team", string="Team Name")
     facilities = fields.Many2many("venue.facilities", string="Facilities")
 
     active = fields.Boolean(string="Active", default=True)
-
-    @api.model
-    def create(self, vals):
-        if vals.get('product_id', _('New')) == _('New'):
-            vals['product_id'] = self.env['ir.sequence'].next_by_code('venue.list') or _('New')
-        res = super(VenueList, self).create(vals)
-        return res
