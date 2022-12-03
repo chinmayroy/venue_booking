63db081fe762f15d0c9f4aaaaddebc1886590267
Chinmay Roy <mrchinmayroy@gmail.com>
2022-12-03 15:53:13 +0600

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import employee_information
-from . import payment_status
 from . import venue_booked_list
 from . import venue_booking_team
 from . import venue_foods
